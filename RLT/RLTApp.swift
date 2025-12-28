f8d89fe6969bf97422ca0199a1d1a32bf6a75113
vincentjohnatan <vincent.johnatan@gmail.com>
2025-12-28 17:38:25 +0200

    Initial Commit

---
+++ RLT/RLTApp.swift
@@ -0,0 +1,17 @@
+//
+//  RLTApp.swift
+//  RLT
+//
+//  Created by Johnatan Vincent on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct RLTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
